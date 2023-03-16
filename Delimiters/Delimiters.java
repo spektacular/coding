98bcb2bce2e9472a80ae90faff1b2299b5aea330
spek <102490408+spektacular@users.noreply.github.com>
2023-03-16 00:00:53 -0700

    Add files via upload

---
+++ Delimiters/Delimiters.java
@@ -0,0 +1,37 @@
+package Delimiters;
+
+import java.util.ArrayList;
+
+public class Delimiters {
+    private String openDel;
+    private String closeDel;
+
+    public Delimiters(String open, String close) {
+        openDel = open;
+        closeDel = close;
+    }
+
+    public ArrayList<String> getDelimitersList(String[] tokens) {
+        ArrayList<String> res = new ArrayList<>();
+        for (String token : tokens) {
+            if (token.equals(this.openDel) || token.equals(this.closeDel)) {
+                res.add(token);
+            }
+        }
+        return res;
+    }
+
+    public boolean isBalanced(ArrayList<String> delimiters) {
+        int openCount = 0;
+        for (String del : delimiters) {
+            if (del.equals(this.openDel)) {
+                openCount++;
+            } else {
+                openCount--;
+            }
+            if (openCount < 0)
+                return false;
+        }
+        return openCount == 0;
+    }
+}
---
+++ Delimiters/DelimitersRunner.java
@@ -0,0 +1,54 @@
+package Delimiters;
+
+import java.util.ArrayList;
+
+public class DelimitersRunner {
+
+    public static void main(String[] args) {
+        Delimiters fun = new Delimiters("(", ")");
+        System.out.println(fun.getDelimitersList(new String[] { "(", "x + y", ")", " * 5" }));
+        Delimiters fun2 = new Delimiters("<q>", "</q>");
+        System.out.println(fun2.getDelimitersList(new String[] { "<q>", "yy", "</q>", "zz", "</q>" }));
+
+        Delimiters fun3 = new Delimiters("<sup>", "</sup>");
+        ArrayList<String> stuff = new ArrayList<String>();
+        stuff.add("<sup>");
+        stuff.add("<sup>");
+        stuff.add("</sup>");
+        stuff.add("<sup>");
+        stuff.add("</sup>");
+        stuff.add("</sup>");
+        System.out.println(fun3.isBalanced(stuff));
+        stuff = new ArrayList<String>();
+        stuff.add("<sup>");
+        stuff.add("</sup>");
+        stuff.add("</sup>");
+        stuff.add("<sup>");
+        System.out.println(fun3.isBalanced(stuff));
+        stuff = new ArrayList<String>();
+        stuff.add("</sup>");
+        System.out.println(fun3.isBalanced(stuff));
+        stuff = new ArrayList<String>();
+        stuff.add("<sup>");
+        stuff.add("<sup>");
+        stuff.add("</sup>");
+        System.out.println(fun3.isBalanced(stuff));
+        stuff = new ArrayList<String>();
+        stuff.add("<sup>");
+        stuff.add("<sup>");
+        stuff.add("</sup>");
+        stuff.add("</sup>");
+        System.out.println(fun3.isBalanced(stuff));
+    }
+}
+/*
+ * output
+ * 
+ * [(, )]
+ * [<q>, </q>, </q>]
+ * true
+ * false
+ * false
+ * false
+ * true
+ */
